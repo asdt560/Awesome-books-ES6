6db586c65471f7850809885bf2beb7aee71af321
JustoRago <103001263+asdt560@users.noreply.github.com>
2022-09-23 18:09:47 -0300

    Add accessibility labels

--- modules/listbuilder.js
+++ modules/listbuilder.js
@@ -4,6 +4,7 @@ const listbuilder = () => {
   arr.forEach((item) => {
     booklist.innerHTML += `<div class="book">
     <p class="title-author">"${item.title}" by ${item.author}</p>
+    <label for="${arr.indexOf(item)}" hidden>Remove "${item.title}"</label>
     <button id=${arr.indexOf(item)} class="remover">Remove</button>
     </div>`;
   });
